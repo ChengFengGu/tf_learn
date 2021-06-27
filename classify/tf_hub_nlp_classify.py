0b2672cb8edb3daada2648085a8492eb102ba0db
GardenLu <lhypersonmail@163.com>
2021-06-27 23:46:36 +0800

    tf_hub nlp classify

--- classify/tf_hub_nlp_classify.py
+++ classify/tf_hub_nlp_classify.py
@@ -15,4 +15,14 @@ import tensorflow_datasets as tfds
 
 #%%
 
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
 
+#%%
+train_data,validation_data,test_data = tfds.load(
+    name="imdb_reviews",
+    split = ('train[:60%]','train[60%:]','test'),
+    as_supervised = True
+)
