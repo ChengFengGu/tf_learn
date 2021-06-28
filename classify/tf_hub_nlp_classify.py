0eae5a22bd895119f81b6a7e86123bd7a88bc6e8
GardenLu <lhypersonmail@163.com>
2021-06-28 11:31:29 +0800

    tf_hub data preprocessin

--- classify/tf_hub_nlp_classify.py
+++ classify/tf_hub_nlp_classify.py
@@ -33,17 +33,58 @@ train_data, validation_data, test_data = tfds.load(
     as_supervised=True,
 )
 
-#%% 
-import os 
+#%%
+import os
+
 os.getcwd()
 
 #%% [markdown]
 # ---
-# 以上的方法需要设置代理，具体可以查看：[tfds下载失败的解决方案](../docs/bugs/tfds_download_fail.md)
-# 
+# 以上的方法需要设置代理，具体可以查看：[tfds下载失败的解决方案](https://github.com/ChengFengGu/tf_learn/blob/master/docs/bugs/tfds_download_fail.md)
+#
 # ---
 
 #%%
 
 train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
+
 train_examples_batch
+
+
+#%% [markdown]
+# ---
+# 可以看到，这个数据是纯粹的文本，没有经过任何预处理。
+#
+# ---
+
+#%%
+
+train_labels_batch
+
+#%% [markdown]
+# ---
+# 构建网络
+# - 词汇要嵌入
+# - 模型有多少层
+# - 多少隐层单元
+#
+# ---
+
+
+#%% [markdown]
+# ---
+# 预训练文本嵌入模型。
+# 常见的有几种：
+# - [google/tf2-preview/gnews-swivel-20dim/1](https://hub.tensorflow.google.cn/google/tf2-preview/gnews-swivel-20dim/1)预训练文本嵌入（text embedding）模型 。
+# - [google/tf2-preview/gnews-swivel-20dim-with-oov/1](https://hub.tensorflow.google.cn/google/tf2-preview/gnews-swivel-20dim-with-oov/1) ——类似 google/tf2-preview/gnews-swivel-20dim/1，但 2.5%的词汇转换为未登录词桶（OOV buckets）。如果任务的词汇与模型的词汇没有完全重叠，这将会有所帮助。
+# - [google/tf2-preview/nnlm-en-dim50/1](https://hub.tensorflow.google.cn/google/tf2-preview/nnlm-en-dim50/1) ——一个拥有约 1M 词汇量且维度为 50 的更大的模型。
+# - [google/tf2-preview/nnlm-en-dim128/1](https://hub.tensorflow.google.cn/google/tf2-preview/nnlm-en-dim128/1) ——拥有约 1M 词汇量且维度为128的更大的模型。
+#
+# ---
+
+#%%
+embedding = "https://hub.tensorflow.google.cn/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
+hub_layer(train_examples_batch[:3])
+
+#%%
